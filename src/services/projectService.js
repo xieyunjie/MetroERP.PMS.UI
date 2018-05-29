f35514335c359957711e76bf934a647100c2b4de
xieyunjie <yunjie.xie@hotmail.com>
2018-05-29 16:33:26 +0800

    add env

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -1,25 +1,26 @@
 import req from '../utils/request';
 //import qs from 'querystring'
+//const pmsUrl = process.env.pmsurl;
 
-export function fetch(searchParsms) {
-  //return req.get(`/pms/Project/ListAll?name=&status=-10&beginDate=2017-11-01T00%3A00%3A00&endDate=2018-12-31T00%3A00%3A00&page=${page}&start=0&limit=50`);
-  return req.get('/pms/Project/ListAll', searchParsms);  
+const pmsUrl = process.env.PMSURL_ENV;
+export function fetch(searchParsms) { 
+  return req.get(`${pmsUrl}/Project/ListAll`, searchParsms);  
 }
-export function search(searchParsms) {
-  return req.get('/pms/Project/ListAll', searchParsms);
+export function search(searchParsms) {  
+  return req.get(`${pmsUrl}/Project/ListAll`, searchParsms);
 }
 
 export function GetMailList(){
-  return req.get(`/pms/MailList/GetMailListByQuery?query=&page=1&start=0&limit=25`);
+  return req.get(`${pmsUrl}/MailList/GetMailListByQuery?query=&page=1&start=0&limit=25`);
 }
 export function getOneProject(id){
-  return req.get('/pms/Project/GetOne', {id});
+  return req.get(`${pmsUrl}/Project/GetOne`, {id});
 }
 
 export function create(values){
-  return req.post('/pms/Project/CreateProject',values); 
+  return req.post(`${pmsUrl}/Project/CreateProject`,values); 
 }
 export function update(values){
-  return req.post('/pms/Project/ModifyProject',values); 
+  return req.post(`${pmsUrl}/Project/ModifyProject`,values); 
 }
 
