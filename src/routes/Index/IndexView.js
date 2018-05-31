29be5c707f3bfa74a78360f54dbfc67db814faeb
xieyunjie <yunjie.xie@hotmail.com>
2018-05-31 16:30:33 +0800

    增加了indexview写法

--- src/routes/Index/IndexView.js
+++ src/routes/Index/IndexView.js
@@ -1,15 +1,53 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { connect } from 'dva'
 import IndexCmp from '../../components/Index/IndexCmp'
 
-function IndexView({index}){
-    const indexProps = {
-        indexValue:index.indexValue
+// function IndexView({index}){
+//     const indexProps = {
+//         indexValue:index.indexValue
+//     }
+//     return (
+//         <IndexCmp {...indexProps}/>
+//     )
+// } 
+
+class IndexView extends Component{
+
+    // constructor(props){
+    //     super(props);
+    // }
+
+    // componentWillMount() {
+    //     console.log('Component WILL MOUNT!')
+    // }
+    componentDidMount() {
+         console.log('Component DID MOUNT!')
     }
-    return (
-        <IndexCmp {...indexProps}/>
-    )
-} 
+    // componentWillReceiveProps(newProps) {
+    //       console.log('Component WILL RECEIVE PROPS!')
+    // }
+    shouldComponentUpdate(newProps, newState) {
+          return true;
+    }
+    // componentWillUpdate(nextProps, nextState) {
+    //       console.log('Component WILL UPDATE!');
+    // }
+    componentDidUpdate(prevProps, prevState) {
+          console.log('Component DID UPDATE!')
+    }
+    componentWillUnmount() {
+           console.log('Component WILL UNMOUNT!')
+    }
+
+    render(){
+     const indexProps = {
+        indexValue:this.props.index.indexValue
+        }
+        return (
+            <IndexCmp {...indexProps}/>
+        )
+    }
+}
 
 function mapStateToProps(state) {   
     const indexModel = state.index;
